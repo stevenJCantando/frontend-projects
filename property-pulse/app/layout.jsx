f0cfac3f503bda17fb32080863d1324383f5990f
Steven John Cantando <sCantand@jwpepper.com>
2024-07-29 14:42:14 -0400

    project setup, nav bar (started)

---
+++ property-pulse/app/layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '@/assets/styles/global.css'
+import Navbar from '@/components/Navbar'
+
+export const metadata = {
+    title: 'Property Pulse | Find The Perfect Rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties'
+}
+
+
+const MainLayout = ( { children }) => {
+  return (
+    <html lang="en">
+        <body>
+            <Navbar />
+            <main>{ children }</main>
+        </body>
+    </html>
+  )
+}
+
+export default MainLayout;
